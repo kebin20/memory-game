88e269c2a1bf9197c3d1bc9c559101c27042da28
kebin20 <ktanzyl@hotmail.com>
2023-05-12 10:31:01 +0900

    style: completed button styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './scss/index.scss';
 function App() {
   return (
     <>
-      <Button className="menu-btn-big">Test Big Button</Button>
+      <Button className="btn btn__menu-big">Big Menu</Button>
+      <Button className="btn btn__menu-selection">Selection</Button>
+      <Button className="btn btn__primary">Primary</Button>
+      <Button className="btn btn__secondary">Secondary</Button>
     </>
   );
 }
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,6 @@
+
+
+{/* <Button className="btn btn__menu-big">Big Menu</Button>
+<Button className="btn btn__menu-selection">Selection</Button>
+<Button className="btn btn__primary">Primary</Button>
+<Button className="btn btn__secondary">Secondary</Button> */}
