580f4cc43ab061881014d97f3db2def72986660f
kebin20 <ktanzyl@hotmail.com>
2023-05-23 09:38:37 +0900

    build: installed nanoid and created number properties

--- src/components/FourByFourGameBoard/FourByFourGameBoard.tsx
+++ src/components/FourByFourGameBoard/FourByFourGameBoard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { CircleButton } from '../ButtonComponents';
 import ScoreBox from '../PlayerScoreBox';
+import { nanoid } from 'nanoid';
 
 function FourByFourGameBoard() {
   const [numbers, setNumbers] = useState(createNumbers());
@@ -8,10 +9,15 @@ function FourByFourGameBoard() {
   function createNumbers() {
     const numberArray = [];
     for (let i = 1; i <= 8; i++) {
-      numberArray.push(i, i);
+      const number = { id: nanoid(), value: i, isFlipped: false };
+      const duplicateNumber = { id: nanoid(), value: i, isFlipped: false };
+
+      numberArray.push(number, duplicateNumber);
     }
     return numberArray;
   }
+
+console.log(numbers)
 
   const circles = numbers.map((number) => (
     <CircleButton className="circle-buttons" key={number}>
